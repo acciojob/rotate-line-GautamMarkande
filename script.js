815b52f4482890885c5014c6474ee8bc1fe31011
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-01 17:40:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-const line = documnet.getElementByid("line");
+const line = document.getElementById("line");
 var degree = 0;
 function rotateLine(){
     degree+=1;
     line.style.transform="rotate("+degree+"deg)";
     requestAnimationFrame(rotateLine)
 }
-rotateline();
+rotateLine();
